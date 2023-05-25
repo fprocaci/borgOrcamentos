f44bb2bfcf40c8c9bb634e61798711434e4f52a7
Fabricio Procaci <fprocaci@gmail.com>
2023-05-25 16:48:56 -0300

    Atualizando o PDF

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Formdata } from '../model/formdata';
 import { Materiais } from '../model/materiais';
 import { Servicos } from '../model/servicos';
-import { jsPDF } from "jspdf";
 import { PdfService } from './pdf.service';
 
 @Component({
@@ -10,31 +9,26 @@ import { PdfService } from './pdf.service';
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.scss'],
 })
-export class FormComponent implements OnInit{
-  formData:Formdata = new Formdata();
-  materiais:Materiais = new Materiais();
+export class FormComponent implements OnInit {
+  formData: Formdata = new Formdata();
+  materiais: Materiais = new Materiais();
   servicos: Servicos = new Servicos();
-  
-  constructor(public pdfService:PdfService){
 
+  constructor(public pdfService: PdfService) {}
 
-  }
-
-  incluiServico(servico: Servicos){
-    this.formData.servicos.push(servico)
+  incluiServico(servico: Servicos) {
+    this.formData.servicos.push(servico);
     this.servicos = new Servicos();
   }
 
-  incluiMateriais(material:Materiais){
+  incluiMateriais(material: Materiais) {
     this.formData.materiais.push(material);
     this.materiais = new Materiais();
   }
 
-  gerarPDF(){
+  gerarPDF() {
     this.pdfService.gerarPDF(this.formData);
   }
-  
-  ngOnInit(): void {
-    
-  }
+
+  ngOnInit(): void {}
 }
--- src/app/form/pdf.service.ts
+++ src/app/form/pdf.service.ts
@@ -1,33 +1,69 @@
 import { Injectable } from '@angular/core';
 import jsPDF from 'jspdf';
 import { Formdata } from '../model/formdata';
+import autoTable from 'jspdf-autotable';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PdfService {
+  constructor() {}
 
-  constructor() { }
-
-  gerarPDF(formData:Formdata){
+  gerarPDF(formData: Formdata) {
+    const imgData = '../../assets/Borginfo.png';
     const doc = new jsPDF();
 
     doc.setFontSize(18);
-    doc.setFont("times","arial","normal");
-    
-    doc.text("Orçamento de Serviço", 75, 10);
-    doc.text("BorgInfo Orçamentos", 75, 20);
+    doc.setFont('times', 'arial', 'normal');
+
+    doc.addImage(imgData, 'PNG', 75, 5, 58, 16);
+    doc.text('Orçamento de Serviço', 75, 35);
 
     doc.setFontSize(16);
-    doc.setFont("times","arial","normal");
-    doc.text("Empresa Responsável", 10 , 40); 
+    doc.setFont('times', 'arial', 'normal');
+    // doc.text('Empresa Responsável', 10, 40);
 
     doc.setFontSize(12);
-    doc.text(`Razão Social:${formData.empresaResponsavel.razaoSocial}`, 10 , 50);
-    doc.text(`Email de contato:${formData.empresaResponsavel.emailDeContato}`, 100 , 50);
-    doc.text(`Celular / WhatsApp:${formData.empresaResponsavel.celularWpp}`, 10 , 60);
-    doc.text(`CPF:${formData.empresaResponsavel.cpf}`, 100 , 60);
+    // autoTable(doc, { html: '#my-table' });
+    autoTable(doc, {
+      margin: { top: 50 },
+      head: [['Empresa Responsável']],
+      body: [
+        [
+          `Razão Social:${formData.empresaResponsavel.razaoSocial}`,
+          `Email de contato:${formData.empresaResponsavel.emailDeContato}`,
+        ],
+        // [
+        //   `Celular / WhatsApp:${formData.empresaResponsavel.celularWpp}`,
+        //   `CPF:${formData.empresaResponsavel.cpf}`,
+        // ],
+        // ...
+      ],
+    });
+
+    autoTable(doc, {
+      styles: { fillColor: [255, 0, 0] },
+      columnStyles: { 0: { halign: 'center', fillColor: [0, 255, 0] } }, // Cells in first column centered and green
+      margin: { top: 10 },
+      body: [
+        [
+          `Razão Social:${formData.empresaResponsavel.razaoSocial}`,
+          `Email de contato:${formData.empresaResponsavel.emailDeContato}`,
+        ],
+        ['Norway', 'China', 'USA'],
+        ['Denmark', 'China', 'Mexico'],
+      ],
+    });
+
+    autoTable(doc, {
+      columnStyles: { europe: { halign: 'center' } }, // European countries centered
+      body: [
+        { europe: 'Sweden', america: 'Canada', asia: 'China' },
+        { europe: 'Norway', america: 'Mexico', asia: 'Japan' },
+      ],
+      columns: [{ header: 'Europe', dataKey: 'europe, asia, america' }, {}],
+    });
+
     doc.save(`Orcamento_${formData.cliente.razaoSocial}.pdf`);
-    
   }
 }
